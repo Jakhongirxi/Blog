919ce79527436562d0f4b04a04e28f8f1a1f5c62
Jakhongir <jakhongirxi@gmail.com>
2024-03-19 15:16:32 +0500

    added urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,7 +3,7 @@ from blog_app import views
 
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('detail/<int:pk>', views.blog_detail, name='blog_detail'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
